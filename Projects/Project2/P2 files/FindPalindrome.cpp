116c17138cf92d8dde67ac1c79e0ca1978aa5aa2
Lucasdt9 <ldt9.pitt@gmail.com>
2021-06-02 23:27:50 -0400

    updated project 2

--- Projects/Project2/P2 files/FindPalindrome.cpp
+++ Projects/Project2/P2 files/FindPalindrome.cpp
@@ -1,7 +1,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <locale> 
+#include <locale>
+#include <algorithm>
 #include "FindPalindrome.hpp"
 
 using namespace std;
@@ -26,8 +27,48 @@ static void convertToLowerCase(string & value)
 void FindPalindrome::recursiveFindPalindromes(vector<string>
         candidateStringVector, vector<string> currentStringVector)
 {
-	// TODO need to implement this recursive function!
-	return;
+	//this will be used later for limits in for loops
+	int rightLimit = currentStringVector.size();
+	//first check to see if candidateStringVector is an empty vector
+	if(candidateStringVector.empty() != false){
+		//if its empty we want to see if the currentStringVector is a palindrome
+		//if it is, we want to add it to the vector
+
+		//these are the recursive function's initial conditions
+		//create an empty string to copy the currentStringVector in to temporarily
+		std::string temp;
+		//create another empty string to contain the end result of the concatenation in lower case
+		std::string tempLowerCase;
+		for(int i = 0; i < rightLimit; i++){
+			//copy the exact string into the temp variable
+			tempExact = currentStringVector.at(i);
+
+			//make it lowercase with given function
+			convertToLowerCase(tempExact);
+
+			//concatenate it to the end of the final result
+			tempLowerCase += tempExact;
+		}
+		//now we want to test our concatenation to see if its actually a palindrome or not
+		if(isPalindrome(tempLowerCase) != false){
+			//if it is a palindrom we're gonna add it to the palinVect and increase the palinFreq
+			//we want to use the original vector of strings not the concatenated version since the original will have spaces between the words
+			palinVect.push_back(currentStringVector);
+			palinFreq++;
+			return 0;
+		}
+		return 0;
+	}
+	//make a loop through the length of the candidateStringVector (rightLimit)
+	for(int i = 0; i < rightLimit; i++){
+		//create temporary variables to be used in the recursion
+		std::vector<std::string> tempCurrent(currentStringVector);
+		std::vector<std::string> tempCandidate(candidateStringVector);
+
+		//need to develop some cutTests before i can finish the recursion
+
+	}
+
 }
 
 // private function to determine if a string is a palindrome (given, you
@@ -51,23 +92,34 @@ bool FindPalindrome::isPalindrome(string currentString) const
 
 FindPalindrome::FindPalindrome()
 {
-	// TODO need to implement this...
+	//set the size of the vector and number of palindromes to 0
+	vectSize = 0;
+	palinFreq = 0;
 }
 
 FindPalindrome::~FindPalindrome()
 {
-	// TODO need to implement this...
+	//no need to use delete here since we aren't using dynamic arrays
+	//running clear() will do just fine
+	clear();
 }
 
 int FindPalindrome::number() const
 {
-	// TODO need to implement this...
-	return 10;
+	// return how many palindromes are in existence
+	return palinFreq;
 }
 
 void FindPalindrome::clear()
 {
-	// TODO need to implement this...
+	//reset all integer variables
+	vectSize = palinFreq = 0;
+
+	//clear vector of sentences uing built in functions for vectors
+	palinSent.clear();
+
+	//clear vector of palindromes using built in functions for vectors
+	palinVect.clear();
 }
 
 bool FindPalindrome::cutTest1(const vector<string> & stringVector)
@@ -97,8 +149,6 @@ bool FindPalindrome::add(const vector<string> & stringVector)
 
 vector< vector<string> > FindPalindrome::toVector() const
 {
-	// TODO need to implement this...
-	vector<vector<string>> returnThingie;
-	return returnThingie;
+	//the return type is a vector of vectors of type string, one vector is a palindrome defined as a vector of strings
+	return palinVect;
 }
-
